4e5edea0dedee71434701d45eeecd2bda2b90142
Sean Dennis <sdennis@geodigital.com>
2021-04-12 16:32:38 -0500

    Forgot to intialize the gane names

--- src/crobes/genetics/genePools/Renderer.java
+++ src/crobes/genetics/genePools/Renderer.java
@@ -1,6 +1,7 @@
 package crobes.genetics.genePools;
 
 import crobes.core.Crobe;
+import crobes.core.CrobeConstants;
 
 public abstract class Renderer extends GenePool implements IRenderGenePool
 {
@@ -14,6 +15,8 @@ public abstract class Renderer extends GenePool implements IRenderGenePool
 
     @Override
     protected void initializeGeneNames() {
-
+        skin().name(CrobeConstants.RENDERER_GENE_SKIN);
+        face().name(CrobeConstants.RENDERER_GENE_FACE);
+        body().name(CrobeConstants.RENDERER_GENE_BODY);
     }
 }
