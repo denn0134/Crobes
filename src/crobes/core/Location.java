1492dd04b8c104268e1fb459bb44da0c15a97aa6
JaroschJr <jaroschjr@gmail.com>
2021-11-11 19:22:03 -0600

    Changed the way of iterating through the Factor list to see if it fixe an error. It did, but it caused a different one.

--- src/crobes/core/Location.java
+++ src/crobes/core/Location.java
@@ -212,12 +212,21 @@ public class Location
             _crobe.world().move(_crobe, direction);
 
         //then factors
+        /*
         for (Factor f: _factors) {
             if (f.world().move(f, direction) == -1) {
                 //remove the Factor if it has left the World
                 f.world().remove(f);
             }//end if
         }//end if
+        */
+        for (int i = 0; i< _factors.size(); i++) {
+            if (_factors.get(i).world().move(_factors.get(i), direction) == -1) {
+                //remove the Factor if it has left the World
+            	_factors.get(i).world().remove(_factors.get(i));
+            }//end if
+        }
+        
     }
 
     @Override
